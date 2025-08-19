41ddfda3dbb0c19f4ca4e0c1d71a8941a7e4da98
Suraj Maurya <152968969+surajmaurya2004@users.noreply.github.com>
2025-08-19 23:25:52 +0530

    Delete web development/responsive landing page directory

--- web development/responsive landing page/script.js
+++
@@ -1,9 +0,0 @@
-// Scroll effect on navbar
-window.addEventListener('scroll', () => {
-  const navbar = document.getElementById('navbar');
-  if (window.scrollY > 50) {
-    navbar.classList.add('scrolled');
-  } else {
-    navbar.classList.remove('scrolled');
-  }
-});
